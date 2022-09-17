833cee67e7ad1ec6f62fd0c9b774c917047ac201
Emilio Pino <pinoen@yahoo.com.ar>
2022-09-17 18:30:52 -0300

    se agregan funciones de validacion para el proceso de signup

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,27 +1,27 @@
 /* ---------------------------------- texto --------------------------------- */
 function validarTexto(texto) {
-    
+
 }
 
 function normalizarTexto(texto) {
-    
+
 }
 
 /* ---------------------------------- email --------------------------------- */
 function validarEmail(email) {
-    
+  return /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/.test(email);
 }
 
 function normalizarEmail(email) {
-    
+
 }
 
 /* -------------------------------- password -------------------------------- */
 function validarContrasenia(contrasenia) {
-    
+  return /(?=.*\W)(?=.*[\w]).{10,15}/.test(contrasenia);
 }
 
 function compararContrasenias(contrasenia_1, contrasenia_2) {
-    
+  return contrasenia_1 === contrasenia_2;
 }
 
