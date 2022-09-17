26529f9c3e719b476163df9432fa229c028ed4e0
Emilio Pino <pinoen@yahoo.com.ar>
2022-09-17 18:31:29 -0300

    se agregan funciones de validacion para el proceso de signup

--- scripts/signup.js
+++ scripts/signup.js
@@ -11,13 +11,20 @@ window.addEventListener('load', function () {
         let apellido = document.querySelector('#inputApellido').value;
         let email = document.querySelector('#inputEmail').value;
         let password = document.querySelector('#inputPassword').value;
+        let reingresoPw = document.querySelector('#inputPasswordRepetida').value;
         let user = {
             firstName: nombre,
             lastName: apellido,
             email: email,
             password: password
         }
-        realizarRegister(user);
+        if (validarEmail(email) && validarContrasenia(password) && compararContrasenias(password, reingresoPw)) {
+            realizarRegister(user);
+        } else {
+            form.innerHTML += `<small class='error'>${"Verifique los datos ingresados."}</small>
+                <small class='error'>${"Todos los campos deben completarse con datos validos."}</small>
+                <small class='error'>${"La contrasena debe tener entre 10 y 15 dígitos alfanuméricos y 1 carácter especial (*,$,&)"}</small>`;
+        }
     });
     /* -------------------------------------------------------------------------- */
     /*                    FUNCIÓN 2: Realizar el signup [POST]                    */
@@ -31,7 +38,6 @@ window.addEventListener('load', function () {
             },
             body: JSON.stringify(newUser)
         }).then(res => res.json()).then(data => {
-            console.log(data);
             localStorage.setItem('token', data.jwt);
             location.replace('./mis-tareas.html');
         })
